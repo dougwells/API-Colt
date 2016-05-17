6d9d2ca9e46a8e695e845a089c9390c04a7784de
Doug Wells <dougwells@gmail.com>
2016-05-17 14:25:49 -0600

    API-Colt. Final Commit

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -9,7 +9,6 @@ app.get('/', function(req, res){
 });
 
 app.get('/results/', function(req, res){
-    
     request('http://www.omdbapi.com/?s='+req.query.s, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var results = JSON.parse(body);
